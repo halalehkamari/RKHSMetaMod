17aceb16a26457ca461e6594045b3f626bd1e3e0
halalehkamari <89902692+halalehkamari@users.noreply.github.com>
2021-09-12 19:58:54 +0200

    Update Prediction.R

--- Prediction.R
+++ Prediction.R
@@ -1,5 +1,8 @@
-prediction <- function(X, Xnew, kernel, Dmax, res, res_nbr){
+prediction <- function(X, Xnew, kernel, Dmax, res, Err){
   Kv.new <- Kv_new(X, Xnew, kernel, Dmax)[[1]]
+  minCritTest <-  which(Err == min(Err, na.rm=TRUE),arr.ind = TRUE)
+  l <- dim(Err)[1]
+  nbr <- l*(minCritTest[2]-1)+minCritTest[1]
   tetavs <- res[[nbr]]$`Meta-Model`$teta 
   fvnew <- Kv.new%*%t(tetavs) 
   fhatnew <- apply(fvnew,1,sum)
